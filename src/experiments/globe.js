1bfcc87eb5e75f32dfa95ae7b02f2df010a28793
Helgard Richard Ferreira <helgard.ferreira@valr.com>
2022-01-11 08:10:38 +0200

    refactor: Refactor globe project

--- src/experiments/globe.js
+++ src/experiments/globe.js
@@ -1,3 +1,5 @@
+// noinspection JSCheckFunctionSignatures
+
 import * as THREE from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
 import createCanvas from "../createCanvas";
@@ -5,42 +7,22 @@ import {DEG2RAD} from "three/src/math/MathUtils";
 import {MeshBasicMaterial} from "three";
 import Stats from 'stats.js'
 
-const fpsStats = new Stats();
-fpsStats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild(fpsStats.dom);
-
-/**
- * Base
- */
-// Canvas
-const canvas = createCanvas();
-
-// Scene
-const scene = new THREE.Scene();
-
-// Globe
-const GLOBE_RADIUS = 1;
-const globeSphere = new THREE.Mesh(
-  new THREE.SphereGeometry(GLOBE_RADIUS, 64, 64),
-  new MeshBasicMaterial({
-    opacity: 0.05,
-    transparent: true,
-    color: 0x0B122E
-  }),
-  // mapMaterial,
-);
-
 class HeroGlobe {
-  dotRadius = (2 * Math.PI * GLOBE_RADIUS) / 1500;
-  rows = 180;
-  dotDensity = 100;
+  globeRadius = 1;
+  // dotRadius = this.globeRadius / 200;
+  dotRadius = this.globeRadius / 250;
+  // dotRadius = this.globeRadius / 300;
+  // rows = 180;
+  rows = 200;
+  // rows = 360;
+  dotDensity = 60;
   dotMatrices = [];
   position = new THREE.Vector3();
   rotation = new THREE.Euler();
   quaternion = new THREE.Quaternion();
   scale = new THREE.Vector3(1, 1, 1);
   group = new THREE.Group();
-  imageData;
+  map = {};
 
   /**
    * HTML Canvas Image Manipulation Logic
@@ -48,23 +30,26 @@ class HeroGlobe {
   initImageData = () => {
     return new Promise((resolve, reject) => {
       try {
-        // Create a canvas element
-        const imageCanvas = document.createElement('canvas');
-        imageCanvas.width = 400;
-        imageCanvas.height = 200;
-
-        // Get the drawing context
-        const imageCtx = imageCanvas.getContext('2d');
-
         // Create a new Image instance
         const img = new Image();
         img.crossOrigin = 'anonymous';
         img.src = '/textures/map.png';
         img.onload = () => {
+          this.map.width = img.width;
+          this.map.height = img.height;
+
+          // Create a canvas element
+          const imageCanvas = document.createElement('canvas');
+          imageCanvas.width = this.map.width;
+          imageCanvas.height = this.map.height;
+
+          // Get the drawing context
+          const imageCtx = imageCanvas.getContext('2d');
+
           imageCtx.drawImage(img, 0, 0);
           img.style.display = 'none';
 
-          this.imageData = imageCtx.getImageData(0, 0, 400, 200);
+          this.map.imageData = imageCtx.getImageData(0, 0, this.map.width, this.map.height);
           resolve();
         };
       } catch (err) {
@@ -77,15 +62,14 @@ class HeroGlobe {
    * Equirectangular map image processing
    */
   isDotVisible = (lat, long) => {
-    // 400 = map width
-    // 200 = map height
-    const x = ((long + 180) * (400 / 360));
-    const y = (((lat * -1) + 90) * (200 / 180));
+    const x = parseInt((long + 180) / 360 * this.map.width);
+    const y = parseInt((-lat + 90) / 180 * this.map.height);
+    // Alternative method for calculating y projection
+    // const y = this.map.height - parseInt((lat + 90) / 180 * this.map.height);
 
-    // 400 = image width
-    const index = (Math.floor(x) + Math.floor(y) * 400) * 4;
-    const alpha = this.imageData.data[index + 3]
-    return alpha >= 90;
+    const index = (x + y * this.map.width) * 4 + 3;
+    const alpha = this.map.imageData.data[index]
+    return alpha > 90;
   }
 
   /**
@@ -95,7 +79,7 @@ class HeroGlobe {
     await this.initImageData();
 
     for (let lat = -90; lat <= 90; lat += 180 / this.rows) {
-      const radius = Math.cos(Math.abs(lat) * DEG2RAD) * GLOBE_RADIUS;
+      const radius = Math.cos(Math.abs(lat) * DEG2RAD) * this.globeRadius;
       const circumference = radius * Math.PI * 2;
       const dotsForLat = circumference * this.dotDensity;
 
@@ -110,9 +94,9 @@ class HeroGlobe {
         const phi = (90 - lat) * DEG2RAD;
         const theta = (long + 180) * DEG2RAD;
 
-        this.position.x = -(GLOBE_RADIUS * Math.sin(phi) * Math.cos(theta));
-        this.position.z = (GLOBE_RADIUS * Math.sin(phi) * Math.sin(theta));
-        this.position.y = (GLOBE_RADIUS * Math.cos(phi));
+        this.position.x = -(this.globeRadius * Math.sin(phi) * Math.cos(theta));
+        this.position.z = (this.globeRadius * Math.sin(phi) * Math.sin(theta));
+        this.position.y = (this.globeRadius * Math.cos(phi));
 
         this.rotation.x = 0;
         this.rotation.y = 0;
@@ -121,10 +105,12 @@ class HeroGlobe {
         this.quaternion.setFromEuler(this.rotation);
 
         const matrix = new THREE.Matrix4();
-        matrix.compose(
-          this.position, this.quaternion, this.scale
-        )
-        matrix.lookAt(this.position, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 1, 0));
+        matrix.compose(this.position, this.quaternion, this.scale)
+        matrix.lookAt(
+          this.position,
+          new THREE.Vector3(0, 0, 0),
+          new THREE.Vector3(0, 1, 0)
+        );
 
         this.dotMatrices.push(matrix);
       }
@@ -150,6 +136,19 @@ class HeroGlobe {
       dotMesh.setMatrixAt(i, dotMatrix);
     })
 
+    // Globe Sphere
+    const globeSphere = new THREE.Mesh(
+      new THREE.SphereGeometry(this.globeRadius, 64, 64),
+      new MeshBasicMaterial({
+        // opacity: 0,
+        // opacity: 0.05,
+        // opacity: 0.5,
+        opacity: 1,
+        transparent: true,
+        color: 0x0B122E,
+      })
+    );
+
     this.group.add(globeSphere);
     this.group.add(dotMesh);
     this.group.rotation.y = 255 * DEG2RAD;
@@ -157,6 +156,16 @@ class HeroGlobe {
 }
 
 function renderScene() {
+  const fpsStats = new Stats();
+  fpsStats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+  document.body.appendChild(fpsStats.dom);
+
+  /**
+   * Base
+   */
+  const canvas = createCanvas();
+  const scene = new THREE.Scene();
+
   const heroGlobe = new HeroGlobe();
   heroGlobe.renderGlobe().then(() => {
     scene.add(heroGlobe.group);
@@ -177,23 +186,21 @@ function renderScene() {
      * Sizes
      */
     const sizes = {
-      width: window.innerWidth,
-      height: window.innerHeight,
+      width: window.innerWidth, height: window.innerHeight,
     };
 
     /**
      * Camera
      */
-      // Base camera
     const camera = new THREE.PerspectiveCamera(
-        75,
-        sizes.width / sizes.height,
-        0.1,
-        100
-      );
+      30,
+      sizes.width / sizes.height,
+      0.1,
+      100
+    );
     camera.position.x = 0;
-    camera.position.y = 0;
-    camera.position.z = 2;
+    camera.position.y = 1;
+    camera.position.z = 5;
     scene.add(camera);
 
     // Controls
@@ -233,7 +240,7 @@ function renderScene() {
       // const elapsedTime = clock.getElapsedTime();
 
       // Bind animation to elapsedTime to reduce FPS inconsistencies
-      heroGlobe.group.rotateY(0.5 * DEG2RAD)
+      heroGlobe.group.rotateY(0.05 * DEG2RAD);
 
       // Update controls
       controls.update();
